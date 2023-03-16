fb252ce597020e8bc3053085b8c6cf1bc9318b89
Ariel Rochistein <ArielRo@imc-grp.com>
2023-03-16 08:07:52 +0200

    day 1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatToolbarModule } from '@angular/material/toolbar';
-import { MatIconModule } from '@angular/material/icon';
-import { MatButtonModule } from '@angular/material/button';
 
 import { HeaderComponent } from './components/header/header.component';
 import { AppComponent } from './app.component';
+import { HomeModule } from './home/home.module';
+import { SharedModule } from './shared/shared.module';
+import { MaterialModule } from './material/material.module';
 
 @NgModule({
   declarations: [AppComponent, HeaderComponent],
@@ -15,9 +15,9 @@ import { AppComponent } from './app.component';
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MatToolbarModule,
-    MatIconModule,
-    MatButtonModule,
+    HomeModule,
+    SharedModule,
+    MaterialModule,
   ],
   providers: [],
   bootstrap: [AppComponent],
--- src/app/home/home.module.ts
+++ src/app/home/home.module.ts
@@ -1,12 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HomeComponent } from './components/home/home.component';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
-  declarations: [
-    HomeComponent
-  ],
-  imports: [CommonModule],
+  declarations: [HomeComponent],
+  imports: [CommonModule, SharedModule],
   exports: [],
 })
 export class HomeModule {}
